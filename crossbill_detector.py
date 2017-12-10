1b41f5b430afcd20b16970429edb3d27db4f0aa1
unknown <rhine3@gmail.com>
2017-12-10 13:33:32 -0500

    Added directory creation in detections_to_files

--- crossbill_detector.py
+++ crossbill_detector.py
@@ -1,6 +1,7 @@
 from old_bird_detector_redux_1_1 import ThrushDetector, TseepDetector
 from audio_file_utils import read_wave_file, write_wave_file
 from bunch import Bunch
+from os import path, makedirs
 import numpy
 
 class _Listener:
@@ -17,35 +18,43 @@ def detections_to_files(samples, detections, sample_rate):
 	their start time and length in samples.
 	
 	- samples: a single-channel numpy array of samples
-	- detections: a list of tuples of detection start time and length in samples 
+	- detections: a list of tuples (detection_start_time, detection_length) 
 	  created by _Listener's method, process_clip
 	- sample_rate: integer sample rate
 	'''
 	
+	# make a "detections" path
+	if not path.exists('detections'):
+		makedirs('detections')
+	
 	for detection in detections:
 		(start, length) = detection
-		path = "detections/clip{}.wav".format(start)
+		
+		filename = "detections/clip{}.wav".format(start)
+		
 		clip = samples[0][start:start+length]
-		write_wave_file(path, clip, sample_rate)
+		write_wave_file(filename, clip, sample_rate)
 		print("{} saved".format(clip))
 		
-def main():		
+def main():
 	# read sample file within directory
 	(samples, sample_rate) = read_wave_file("sample.wav")
 	print(samples)
 	
-	# only feed one channel into ThrushDetector
+	listener = _Listener()
+	
+	# if samples is 2D, only feed first subarray into ThrushDetector
 	if len(samples) > 1:
+		assert samples[0].all() == samples[1].all()
 		sample = samples[0]
 	else:
 		sample = samples
-	
-	listener = _Listener()
 	
 	detector = ThrushDetector(sample_rate, listener)
 	detector.detect(sample)
 	detector.complete_detection()
 
+	
 	detections_to_files(samples, listener.clips, sample_rate)
 
 main()
